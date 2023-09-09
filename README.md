b7bd26ffa9e74483dbcb2e890a5eedb90c37e7ab
yuta <yu.code.tech@gmail.com>
2023-09-09 20:33:22 +0900

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Template Static
+
+## 開発環境
+以下の環境で動作を確認しています。
+- node v-18.16
+- npm v-9.5.1
+
+## ビルドツール
+
+- gulp
+
+## gulp コマンド
+
+```
+# パッケージのダウンロード
+$ npm i
+
+# コンパイル
+$ npx gulp
+
+# webp変換
+$ npx gulp webp
+```
+
+## ファイル構造
+
+- dist/・・・メインディレクトリ
+- sass/・・・Sass ファイル群
+- img/・・・Src画像ファイル群
+- package.json・・・Glup パッケージ
+- gulpfile.js・・・Glup タスク
+- .editorconfig・・・エディタルール
+- .stylelintrc.json・・・Sass コーディングルール
---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+const { src, dest, series, watch, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const gcmq = require("gulp-group-css-media-queries");
+const notify = require("gulp-notify");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssdeclsort = require("css-declaration-sorter");
+const sassGlob = require("gulp-sass-glob-use-forward");
+const webp = require("gulp-webp");
+const del = require("del");
+const browserSync = require("browser-sync");
+
+// パスの定義
+const distBase = "./dist";
+const srcSass = "./scss/**/*.scss";
+const srcImg = "./img/**";
+const distCss = `${distBase}/assets/css`;
+const distImg = `${distBase}/assets/img`;
+const distFile = `${distBase}/**/*`;
+
+// Sassコンパイル
+const compileSass = (done) => {
+  src(srcSass)
+    .pipe(
+      plumber({
+        errorHandler: notify.onError("Error:<%= error.message %>"),
+      })
+    )
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(
+      postcss([
+        cssdeclsort({
+          order: "alphabetical",
+        }),
+      ])
+    )
+    .pipe(gcmq())
+    .pipe(dest(distCss));
+  done();
+};
+
+// ローカルサーバー
+const browserSyncFunc = (done) => {
+  browserSync.init({
+    server: { baseDir: distBase },
+  });
+  done();
+};
+
+const browserSyncReload = (done) => {
+  browserSync.reload();
+  done();
+};
+
+// 変更の監視
+const watchFiles = (done) => {
+  watch(srcSass, series(compileSass, browserSyncReload));
+  watch(srcImg, series(clean, copyImages, browserSyncReload));
+  watch(distFile, browserSyncReload);
+  done();
+};
+
+// 画像ファイルリセット
+const clean = () => {
+  return del(distImg, {
+    force: true,
+  });
+};
+
+// 画像をコピー
+const copyImages = (done) => {
+  src(srcImg).pipe(dest(distImg));
+  done();
+};
+
+//webP変換
+function imageWebp() {
+  return src([`${srcImg}/**.png`, `${srcImg}/**.jpg`, `${srcImg}/**.jpeg`])
+    .pipe(
+      webp({
+        quality: 90,
+        method: 6,
+      })
+    )
+    .pipe(dest(`${distImg}`));
+}
+
+// タスク実行
+exports.webp = imageWebp;
+exports.default = series(watchFiles, browserSyncFunc);
