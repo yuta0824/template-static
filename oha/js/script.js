5c7f5fbe3e73111346f001e37fd67bbc680ebc18
yuta <yu.code.tech@gmail.com>
2024-11-26 02:52:39 +0900

    index.htmlとindex.phpの構造を整理し、不要なsrcファイルを削除。lintスクリプトを更新し、ボタンのクリックアラートメッセージを修正。_check.scssのスタイルを調整。

---
+++ oha/js/script.js
@@ -0,0 +1,99 @@
+jQuery("#js-drawer-icon").on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").toggleClass("is-checked");
+  jQuery("#js-drawer-content").toggleClass("is-checked");
+});
+
+// const drawerIcon = document.querySelector("#js-drawer-icon");
+// const drawerContent = document.querySelector("#js-drawer-content");
+
+// if (drawerIcon) {
+//   drawerIcon.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     drawerIcon.classList.toggle("is-checked");
+//     drawerContent.classList.toggle("is-checked");
+//   });
+// }
+
+jQuery(".js-accordion").on("click", function (e) {
+  e.preventDefault();
+
+  if (jQuery(this).parent().hasClass("is-open")) {
+    jQuery(this).parent().removeClass("is-open");
+    jQuery(this).next().slideUp();
+  } else {
+    jQuery(this).parent().addClass("is-open");
+    jQuery(this).next().slideDown();
+  }
+});
+
+const swiper = new Swiper("#js-gallery-swiper", {
+  spaceBetween: 82,
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: "#js-gallery-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#js-gallery-next",
+    prevEl: "#js-gallery-prev",
+  },
+});
+
+jQuery(".js-modal-open").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].showModal();
+});
+
+jQuery(".js-modal-close").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].close();
+});
+
+jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
+
+jQuery('a[href^="#"]').on("click", function (e) {
+  const speed = 300;
+  const id = jQuery(this).attr("href");
+  const target = jQuery("#" == id ? "html" : id);
+  const position = jQuery(target).offset().top;
+  jQuery("html, body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing" // swing or linear
+  );
+});
+
+jQuery(window).on("scroll", function () {
+  if (100 < jQuery(window).scrollTop()) {
+    jQuery("#js-pagetop").addClass("is-show");
+  } else {
+    jQuery("#js-pagetop").removeClass("is-show");
+  }
+});
+
+const intersectionObserver = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-in-view");
+    } else {
+      // entry.target.classList.remove("is-in-view");
+    }
+  });
+});
+
+const inViewItems = document.querySelectorAll(".js-in-view");
+inViewItems.forEach(function (inViewItem) {
+  intersectionObserver.observe(inViewItem);
+});
